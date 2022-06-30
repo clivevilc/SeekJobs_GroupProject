dd1ef310eda8c38139da562c4bcec5d2ba84af6b
clive-jpg <kamchiyiu@gmail.com>
2022-06-30 19:43:12 +0800

    Login Auth // Login Router

--- db/userAuthQueries.js
+++ db/userAuthQueries.js
@@ -1,1 +1,6 @@
 //user queries that could link 
+const development = require("./knexfile").development;
+const knex = require("knex")(development);
+const hashFunction = require("../passport/hashFunction")
+const TABLE_NAME = "credentials"
+
